01be059f8810b46c892929af9dbf7605a071a6fd
Allen Hsieh <10136383+allenhsieh@users.noreply.github.com>
2023-04-01 00:29:45 -0500

    edit crawl to just use axios and cheerio

--- index.js
+++ index.js
@@ -1,33 +1,35 @@
 const axios = require('axios');
 const cheerio = require('cheerio');
-const { spawnSync } = require('child_process');
 
 const url = 'https://www.theendhtx.com/';
 
-axios.get(url)
-  .then((response) => {
+async function crawl() {
+  try {
+    const response = await axios.get(url);
     const $ = cheerio.load(response.data);
-    const links = [];
-
-    $('a[data-hook="ev-rsvp-button"]').each((i, elem) => {
-      const href = $(elem).attr('href');
-      if (href) {
-        links.push(href);
-      }
+    const rsvpButtons = $('a[data-hook="ev-rsvp-button"]');
+    const hrefs = [];
+    rsvpButtons.each((i, elem) => {
+      hrefs.push($(elem).attr('href'));
     });
+    console.log(hrefs);
 
-    links.forEach((link) => {
-      const command = 'curl';
-      const args = [
-        '-X', 'POST',
-        '-H', 'Accept: application/json',
-        '-H', `Authorization: LOW ${process.env.ACCESS_KEY}:${process.env.SECRET}`,
-        '-d', `url=${link}`,
-        'https://web.archive.org/save',
-      ];
-      spawnSync(command, args, { stdio: 'inherit' });
-    });
-  })
-  .catch((error) => {
+    for (let i = 0; i < hrefs.length; i++) {
+      await axios.post('https://web.archive.org/save', {
+        url: hrefs[i]
+      }, {
+        headers: {
+          'Accept': 'application/json',
+          'Authorization': 'LOW myaccesskey:mysecret'
+        }
+      });
+    }
+
+    console.log('Crawling completed!');
+  } catch (error) {
     console.error(error);
-  });
+  }
+}
+
+crawl();
+
